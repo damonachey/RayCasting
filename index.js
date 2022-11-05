442d047149678ef8afd3aa182d2d8c38763a794c
Damon Achey <damon@achey.net>
2022-11-05 16:26:46 -0600

    added mousemove event listener

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import { RegularPolygon } from './js/regularPolygon.js';
 import { Pacman } from './js/pacman.js';
 
+const view = document.getElementById('view');
 const app = new PIXI.Application({
-    view: document.getElementById('view'),
+    view: view,
     width: 800,
     height: 800,
     backgroundColor: 0x1e1e1e,
@@ -12,9 +13,15 @@ const app = new PIXI.Application({
 let pause = false;
 let wireFrame = false;
 
+let mouse = { x: 0, y: 0 };
+
 window.addEventListener('keydown', function (event) {
     if (event.key == 'p') pause = !pause;
     if (event.key == 'w') wireFrame = !wireFrame;
+});
+
+view.addEventListener('mousemove', function(event){
+    mouse = { x: event.clientX, y: event.clientY };
 });
 
 const g = new PIXI.Graphics();
@@ -22,11 +29,14 @@ app.stage.addChild(g);
 
 const color = 0x0000ff;
 
+const corners = { points: [{ x: 0, y: 0 }, { x: view.width, y: 0 }, { x: view.width, y: view.height }, { x: 0, y: view.height }] };
 const triangle = new RegularPolygon({ facets: 3 }).scale({ scale: 100 }).translate({ x: 200, y: 200 });
 const square = new RegularPolygon({ facets: 4 }).scale({ scale: 100 }).translate({ x: 600, y: 200 });
 const pentagon = new RegularPolygon({ facets: 5 }).scale({ scale: 100 }).translate({ x: 200, y: 600 });
 const circle = new RegularPolygon({ facets: 60 }).scale({ scale: 100 }).translate({ x: 600, y: 600 });
 const pacman = new Pacman({ facets: 60 }).scale({ scale: 100 }).translate({ x: 400, y: 400 });
+
+const polygons = [triangle, square, pentagon, circle, pacman];
 
 app.ticker.add(() => {
     if (pause) return;
@@ -38,11 +48,7 @@ app.ticker.add(() => {
         g.beginFill(color);
     }
 
-    triangle.draw(g);
-    square.draw(g);
-    pentagon.draw(g);
-    circle.draw(g);
-    pacman.draw(g);
+    polygons.forEach((polygon) => polygon.draw(g));
 
     if (!wireFrame) {
         g.endFill();
