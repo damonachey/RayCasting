7ce34ad64075f711f663ca88232d06ab274dd5ab
Damon Achey <damon@achey.net>
2022-11-06 01:41:26 -0600

    refactored everything

--- index.js
+++ index.js
@@ -4,56 +4,16 @@ import { Segment } from './js/segment.js';
 import { Point } from './js/point.js';
 import { Polygon } from './js/polygon.js';
 
-const canvasCanvas = document.getElementById('canvasCanvas');
-const canvasFps = document.getElementById('canvasFps');
+const width = 600;
+const height = 600;
 
-const pixiCanvas = document.getElementById('pixiCanvas');
-const pixiFps = document.getElementById('pixiFps');
-
-const pixiBlue = 0x0000ff;
-const pixiGreen = 0x00ff00;
-const pixiWhite = 0xffffff;
-const pixiBackground = 0x1e1e1e;
-const canvasBlue = '#00f';
-const canvasGreen = '#0f0';
-const canvasWhite = '#fff';
-const canvasBackground = '#1e1e1e';
+const blue = '#00f';
+const green = '#0f0';
+const white = '#fff';
+const background = '#1e1e1e';
 const justABigNumber = 1000000;
 
-const app = new PIXI.Application({
-    view: pixiCanvas,
-    width: canvasCanvas.width,
-    height: canvasCanvas.height,
-    backgroundColor: pixiBackground,
-    antialias: true,
-});
-
-let canvasRect = canvasCanvas.getBoundingClientRect();
-let pixiRect = pixiCanvas.getBoundingClientRect();
-
-let pause = false;
-let wireFrame = false;
-let mouse = new Point(1, 1);
-let canvasLastTime = performance.now();
-
-window.addEventListener('resize', event => {
-    canvasRect = canvasCanvas.getBoundingClientRect();
-    pixiRect = pixiCanvas.getBoundingClientRect();
-    p5Rect = p5Canvas.getBoundingClientRect();
-});
-
-window.addEventListener('keydown', event => {
-    if (event.key == 'p') pause = !pause;
-    if (event.key == 'w') wireFrame = !wireFrame;
-});
-
-canvasCanvas.addEventListener('mousemove', event =>
-    mouse = new Point(event.clientX - canvasRect.left, event.clientY - canvasRect.top));
-
-pixiCanvas.addEventListener('mousemove', event =>
-    mouse = new Point(event.clientX - pixiRect.left, event.clientY - pixiRect.top));
-
-const corners = new Polygon([new Point(0, 0), new Point(canvasCanvas.width, 0), new Point(canvasCanvas.width, canvasCanvas.height), new Point(0, canvasCanvas.height)]);
+const corners = new Polygon([new Point(0, 0), new Point(width, 0), new Point(width, height), new Point(0, height)]);
 const triangle = new RegularPolygon(3).scale(75).translate(150, 150);
 const square = new RegularPolygon(4).scale(75).translate(450, 150);
 const pentagon = new RegularPolygon(5).scale(75).translate(150, 450);
@@ -65,189 +25,247 @@ const polygonSegments = [];
 
 polygons.forEach(polygon => polygonSegments.push(...polygon.segments()));
 
-const ctx = canvasCanvas.getContext('2d');
-updateCanvas();
+let pause = false;
+let filled = true;
+let mouse = new Point(1, 1);
+let canvasLastTime = performance.now();
 
-const graphics = new PIXI.Graphics();
-app.stage.addChild(graphics);
-app.ticker.add(updatePixi, PIXI.UPDATE_PRIORITY.LOW);
-app.ticker.start();
+window.addEventListener('keydown', event => {
+    if (event.key.toLowerCase() == 'p') pause = !pause;
+    if (event.key.toLowerCase() == 'f') filled = !filled;
+});
 
-const p5Fps = document.getElementById('p5Fps');
-let p5Rect;
-new p5(function (p5context) {
-    p5context.setup = function setup() {
-        p5context.createCanvas(canvasCanvas.width, canvasCanvas.height).parent('p5Parent');
-        p5Rect = p5context.canvas.getBoundingClientRect();
-        p5context.canvas.addEventListener('mousemove', event =>
-            mouse = new Point(event.clientX - p5Rect.left, event.clientY - p5Rect.top));
-    }
+(function startCanvas() {
+    const canvas = document.getElementById('canvasCanvas');
+    const fps = document.getElementById('canvasFps');
 
-    p5context.draw = function updateP5() {
+    let rect = canvas.getBoundingClientRect();
+    canvas.addEventListener('mousemove', event => mouse = new Point(event.clientX - rect.left, event.clientY - rect.top));
+    window.addEventListener('resize', () => rect = canvas.getBoundingClientRect());
+
+    const context = canvas.getContext('2d');
+    update(); // kick start for requestAnimationFrame
+
+    function update() {
+        requestAnimationFrame(update);
+
         if (pause) return;
-        if (mouse.x > canvasCanvas.width) return;
-        if (mouse.y > canvasCanvas.height) return;
+        if (mouse.x > canvas.width) return;
+        if (mouse.y > canvas.height) return;
 
         const rays = getRays();
 
-        p5context
-            .clear()
-            .background(canvasBackground)
-            .noFill();
+        context.fillStyle = canvas.style.borderColor;
+        context.rect(0, 0, canvas.width, canvas.height);
+        context.fill();
 
-        p5context.stroke(canvasBlue);
-        if (!wireFrame) {
-            p5context.fill(canvasBlue);
-        }
         polygons.forEach(polygon => {
-            p5context.beginShape();
-            polygon.points.forEach(point => p5context.vertex(point.x, point.y));
-            p5context.endShape(p5context.CLOSE);
+            context.fillStyle = blue;
+            context.strokeStyle = context.fillStyle;
+            context.beginPath();
+            context.moveTo(polygon.points[0].x, polygon.points[0].y);
+
+            polygon.points.forEach(point => context.lineTo(point.x, point.y));
+
+            context.closePath();
+
+            if (filled) {
+                context.fill();
+            }
+
+            context.stroke();
         });
 
-        p5context.stroke(canvasWhite);
-        if (!wireFrame) {
-            p5context.fill(canvasWhite);
+        for (let i = 0; i < rays.length; i++) {
+            context.fillStyle = white;
+            context.strokeStyle = context.fillStyle;
+            context.beginPath();
+            context.moveTo(mouse.x, mouse.y);
+            context.lineTo(rays[i].point2.x, rays[i].point2.y);
+
+            if (filled) {
+                const nextRay = rays[i + 1] || rays[0];
+
+                context.lineTo(nextRay.point2.x, nextRay.point2.y);
+                context.closePath();
+                context.fill();
+            }
+
+            context.stroke();
         }
+
+        context.fillStyle = green;
+        context.strokeStyle = context.fillStyle;
+        context.beginPath();
+        context.ellipse(mouse.x, mouse.y, 10, 10, 0, Math.PI * 2, false);
+
+        if (filled) {
+            context.fill();
+        }
+
+        context.stroke();
+
+        const now = performance.now();
+        fps.innerHTML = Math.round(1000 / (now - canvasLastTime)).toFixed(0);
+        canvasLastTime = now;
+    }
+})();
+
+(function startPixi() {
+    // pixi uses binary color values
+    const blue = 0x0000ff;
+    const green = 0x00ff00;
+    const white = 0xffffff;
+    const background = 0x1e1e1e;
+
+    const canvas = document.getElementById('pixiCanvas');
+    const fps = document.getElementById('pixiFps');
+
+    let rect = canvas.getBoundingClientRect();
+    canvas.addEventListener('mousemove', event => mouse = new Point(event.clientX - rect.left, event.clientY - rect.top));
+    window.addEventListener('resize', () => rect = canvas.getBoundingClientRect());
+
+    const graphics = new PIXI.Graphics();
+    const app = new PIXI.Application({
+        view: canvas,
+        width: canvas.width,
+        height: canvas.height,
+        backgroundColor: background,
+        antialias: true,
+    });
+    app.stage.addChild(graphics);
+    app.ticker.add(update, PIXI.UPDATE_PRIORITY.LOW);
+    app.ticker.start();
+
+    function update() {
+        if (pause) return;
+        if (mouse.x > canvas.width) return;
+        if (mouse.y > canvas.height) return;
+
+        const rays = getRays();
+
+        graphics
+            .clear()
+            .lineStyle(1, blue);
+
+        if (filled) {
+            graphics.beginFill(blue);
+        }
+
+        polygons.forEach(polygon => graphics.drawPolygon(polygon.points));
+
+        graphics.lineStyle(1, white);
+
         for (var i = 0; i < rays.length; i++) {
             const ray = rays[i];
 
-            if (!wireFrame) {
+            if (filled) {
                 const nextRay = rays[i + 1] || rays[0];
-    
-                p5context.fill(pixiWhite);
-                p5context.beginShape();
-                [mouse, ray.point2, nextRay.point2].forEach(point => p5context.vertex(point.x, point.y));
-                p5context.endShape(p5context.CLOSE);
+
+                graphics
+                    .beginFill(white)
+                    .drawPolygon([mouse, ray.point2, nextRay.point2]);
             }
             else {
-                p5context.line(ray.point1.x, ray.point1.y, ray.point2.x, ray.point2.y);
+                graphics
+                    .moveTo(ray.point1.x, ray.point1.y)
+                    .lineTo(ray.point2.x, ray.point2.y);
             }
         }
 
-        p5context.stroke(canvasGreen);
-        if (!wireFrame) {
-            p5context.fill(canvasGreen);
-        }
-        p5context.circle(mouse.x, mouse.y, 20);
-
-        p5Fps.innerText = p5context.frameRate().toFixed(0);
-    };
-});
-
-function updateCanvas() {
-    requestAnimationFrame(updateCanvas);
-
-    if (pause) return;
-    if (mouse.x > canvasCanvas.width) return;
-    if (mouse.y > canvasCanvas.height) return;
-
-    const rays = getRays(mouse);
-
-    ctx.fillStyle = canvasCanvas.style.borderColor;
-    ctx.rect(0, 0, canvasCanvas.width, canvasCanvas.height);
-    ctx.fill();
-
-    // draw polygons
-    polygons.forEach(polygon => {
-        ctx.fillStyle = canvasBlue;
-        ctx.strokeStyle = ctx.fillStyle;
-        ctx.beginPath();
-        ctx.moveTo(polygon.points[0].x, polygon.points[0].y);
-
-        polygon.points.forEach(point => ctx.lineTo(point.x, point.y));
-
-        ctx.closePath();
-
-        if (!wireFrame) {
-            ctx.fill();
+        if (filled) {
+            graphics.beginFill(green);
         }
 
-        ctx.stroke();
+        graphics
+            .lineStyle(1, green)
+            .drawCircle(mouse.x, mouse.y, 10);
+
+        if (filled) {
+            graphics.endFill();
+        }
+
+        fps.innerHTML = app.ticker.FPS.toFixed(0);
+    }
+}());
+
+(function startP5() {
+    let canvas;
+    const fps = document.getElementById('p5Fps');
+
+    new p5(function (p5context) {
+        p5context.setup = function setup() {
+            p5context
+                .createCanvas(width, height)
+                .parent('p5Canvas');
+
+            canvas = p5context.canvas;
+
+            canvas.style.border = '1px solid ' + background;
+
+            let p5Rect = canvas.getBoundingClientRect();
+            canvas.addEventListener('mousemove', event => mouse = new Point(event.clientX - p5Rect.left, event.clientY - p5Rect.top));
+            window.addEventListener('resize', () => p5Rect = canvas.getBoundingClientRect());
+        }
+
+        p5context.draw = function update() {
+            if (pause) return;
+            if (mouse.x > canvas.width) return;
+            if (mouse.y > canvas.height) return;
+
+            const rays = getRays();
+
+            p5context
+                .clear()
+                .background(background)
+                .noFill();
+
+            p5context.stroke(blue);
+
+            if (filled) {
+                p5context.fill(blue);
+            }
+
+            polygons.forEach(polygon => {
+                p5context.beginShape();
+                polygon.points.forEach(point => p5context.vertex(point.x, point.y));
+                p5context.endShape(p5context.CLOSE);
+            });
+
+            p5context.stroke(white);
+
+            if (filled) {
+                p5context.fill(white);
+            }
+
+            for (var i = 0; i < rays.length; i++) {
+                const ray = rays[i];
+
+                if (filled) {
+                    const nextRay = rays[i + 1] || rays[0];
+
+                    p5context.fill(white);
+                    p5context.beginShape();
+                    [mouse, ray.point2, nextRay.point2].forEach(point => p5context.vertex(point.x, point.y));
+                    p5context.endShape(p5context.CLOSE);
+                }
+                else {
+                    p5context.line(ray.point1.x, ray.point1.y, ray.point2.x, ray.point2.y);
+                }
+            }
+
+            p5context.stroke(green);
+            
+            if (filled) {
+                p5context.fill(green);
+            }
+
+            p5context.circle(mouse.x, mouse.y, 20);
+
+            fps.innerText = p5context.frameRate().toFixed(0);
+        };
     });
-
-    // draw rays in order as lines or filled
-    for (let i = 0; i < rays.length; i++) {
-        ctx.fillStyle = canvasWhite;
-        ctx.strokeStyle = ctx.fillStyle;
-        ctx.beginPath();
-        ctx.moveTo(mouse.x, mouse.y);
-        ctx.lineTo(rays[i].point2.x, rays[i].point2.y);
-
-        if (!wireFrame) {
-            const nextRay = rays[i + 1] || rays[0];
-
-            ctx.lineTo(nextRay.point2.x, nextRay.point2.y);
-            ctx.closePath();
-            ctx.fill();
-        }
-
-        ctx.stroke();
-    }
-
-    // draw light circle at mouse
-    ctx.fillStyle = canvasGreen;
-    ctx.strokeStyle = ctx.fillStyle;
-    ctx.beginPath();
-    ctx.ellipse(mouse.x, mouse.y, 10, 10, 0, Math.PI * 2, false);
-    if (!wireFrame) ctx.fill();
-    ctx.stroke();
-
-    const now = performance.now();
-    canvasFps.innerHTML = Math.round(1000 / (now - canvasLastTime)).toFixed(0);
-    canvasLastTime = now;
-}
-
-function updatePixi() {
-    if (pause) return;
-    if (mouse.x > canvasCanvas.width) return;
-    if (mouse.y > canvasCanvas.height) return;
-
-    const rays = getRays();
-
-    graphics
-        .clear()
-        .lineStyle(1, pixiBlue);
-
-    if (!wireFrame) {
-        graphics.beginFill(pixiBlue);
-    }
-
-    polygons.forEach(polygon => graphics.drawPolygon(polygon.points));
-
-    graphics.lineStyle(1, pixiWhite);
-
-    for (var i = 0; i < rays.length; i++) {
-        const ray = rays[i];
-
-        if (!wireFrame) {
-            const nextRay = rays[i + 1] || rays[0];
-
-            graphics
-                .beginFill(pixiWhite)
-                .drawPolygon([mouse, ray.point2, nextRay.point2]);
-        }
-        else {
-            graphics
-                .moveTo(ray.point1.x, ray.point1.y)
-                .lineTo(ray.point2.x, ray.point2.y);
-        }
-    }
-
-    if (!wireFrame) {
-        graphics.beginFill(pixiGreen);
-    }
-
-    graphics
-        .lineStyle(1, pixiGreen)
-        .drawCircle(mouse.x, mouse.y, 10);
-
-    if (!wireFrame) {
-        graphics.endFill();
-    }
-
-    pixiFps.innerHTML = app.ticker.FPS.toFixed(0);
-}
+}());
 
 function getRays() {
     const polygonPoints = [...corners.points];
@@ -276,7 +294,5 @@ function getRays() {
         rays.push(shortest);
     });
 
-    rays.sort((a, b) => a.angle() - b.angle());
-
-    return rays;
+    return rays.sort((a, b) => a.angle() - b.angle());
 }
